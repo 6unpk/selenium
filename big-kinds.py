0572231bda26a2c26f5d725c7b8089e75491aa48
Junu Park <pkjoho95@gmail.com>
2024-03-19 11:29:39 +0900

    fix big-kinds.py

--- big-kinds.py
+++ big-kinds.py
@@ -17,7 +17,7 @@ element = wait.until(EC.element_to_be_clickable((By.ID, 'resulteRe-btn')))
 
 result = []
 
-for page in range(1, 2000):
+for page in range(2, 2000):
     page_input = browser.find_element(By.ID, 'paging_news_result')
     newsResult = browser.find_element(By.ID, 'news-results')
     newsTitle = newsResult.find_elements(By.CLASS_NAME, 'title-elipsis')
@@ -25,6 +25,9 @@ for page in range(1, 2000):
     for title in newsTitle:
         result.append(title.text)
 
+    page_input.send_keys(Keys.BACK_SPACE)
+    page_input.send_keys(Keys.BACK_SPACE)
+    page_input.send_keys(Keys.BACK_SPACE)
     page_input.send_keys(Keys.BACK_SPACE)
     page_input.send_keys(f'{page}')
     page_input.send_keys(Keys.ENTER)
