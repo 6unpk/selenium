a4291ceeaebf74626821cb6da96cd0bcd626d55d
Junu Park <pkjoho95@gmail.com>
2024-03-21 18:54:35 +0900

    fix naver-news.py

--- example/naver-news.py
+++ example/naver-news.py
@@ -11,7 +11,7 @@ browser = webdriver.Chrome()
 
 keyword = input('검색 키워드:')
 encoded = urllib.parse.quote_plus(keyword)
-browser.get(f'https://search.naver.com/search.naver?where=nexearch&sm=top_sug.pre&fbm=0&acr=1&acq=%EC%9D%98&qdt=0&ie=utf8&query={encoded}')
+browser.get(f'https://search.naver.com/search.naver?ssc=tab.news.all&where=news&sm=tab_jum&query={encoded}')
 
 
 fields_name = ['제목', 'URL', '언론사']
@@ -20,12 +20,12 @@ news_result = []
 with open('news_result.csv', 'w') as news_csv:
     csv_writer = csv.writer(news_csv, delimiter=',', quotechar=',')
     for i in range(10):
-        list_news = browser.find_elements(By.CLASS_NAME, 'list_news')[i * 10: i * 10 + 10]
+        list_news = browser.find_element(By.CLASS_NAME, 'list_news').find_elements(By.CLASS_NAME, 'bx')[i * 10: i * 10 + 10]
         # 10개씩 더 가져온다.
         for news in list_news:
             news_title = news.find_element(By.CLASS_NAME, 'news_tit').get_attribute('title')
             news_url = news.find_element(By.CLASS_NAME, 'news_tit').get_attribute('href')
-            press = news.find_element(By.CLASS_NAME, '').get_attribute('')
+            press = news.find_element(By.CLASS_NAME, 'press').text
             csv_writer.writerow([news_title, news_url, press])
 
         browser.execute_script('window.scrollTo(0, document.body.scrollHeight)')
