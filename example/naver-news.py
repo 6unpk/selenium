cbe148c94d786ac27f88139403ac00944d2c10ac
Junu Park <pkjoho95@gmail.com>
2024-03-30 18:50:51 +0900

    fix naver-news.py

--- example/naver-news.py
+++ example/naver-news.py
@@ -4,7 +4,6 @@ import time
 import urllib.parse
 import csv
 
-
 browser = webdriver.Chrome()
 
 keyword = input('검색 키워드:')
@@ -15,10 +14,11 @@ browser.get(f'https://search.naver.com/search.naver?ssc=tab.news.all&where=news&
 fields_name = ['제목', 'URL', '언론사']
 news_result = []
 
-with open('news_result.csv', 'w') as news_csv:
+with open('news_result.csv', 'w', encoding='utf-8') as news_csv:
     csv_writer = csv.writer(news_csv, delimiter=',', quotechar=',')
-    for i in range(10):
-        list_news = browser.find_element(By.CLASS_NAME, 'list_news').find_elements(By.CLASS_NAME, 'bx')[i * 10: i * 10 + 10]
+
+    for i in range(1):
+        list_news = browser.find_element(By.CLASS_NAME, 'list_news').find_elements(By.CLASS_NAME, 'bx')[i * 10: i * 10 + 2]
         # 10개씩 더 가져온다.
         for news in list_news:
             news_title = news.find_element(By.CLASS_NAME, 'news_tit').get_attribute('title')
@@ -30,14 +30,38 @@ with open('news_result.csv', 'w') as news_csv:
             press = news.find_element(By.CLASS_NAME, 'press').text
             original_window = browser.current_window_handle
             time.sleep(0.1)
+            # 네이버 뉴스 기사 페이지로 창을 이동하는 부분
             if type(news_url) == str:
-                browser.switch_to.new_window('tab')
-                browser.get(news_url)
+                browser.switch_to.new_window('tab') # 새로운 브라우저 탭을 생성
+                browser.get(news_url) # 네이버 뉴스 기사로 이동
                 article = browser.find_element(By.ID, 'dic_area').text
-                csv_writer.writerow([news_title, news_url, press, article])
+                try:
+                    comment_button = browser.find_element(By.CLASS_NAME, 'media_end_head_cmtcount_button')
+                    comment_count = comment_button.text
+                    comment_button.click()
+                except:
+                    pass
+
+
+                time.sleep(1)
+                try:
+                    while True:
+                        browser.find_element(By.CLASS_NAME, 'u_cbox_more_wrap').click()
+                        time.sleep(0.5)
+                except:
+                    pass
+
+                chats = browser.find_elements(By.CLASS_NAME, 'u_cbox_contents')
+
+                news_chat = []
+                for chat in chats:
+                    news_chat.append(chat.text)
+
+                csv_writer.writerow([news_title, news_url, press, article, str(news_chat)])
                 browser.close()
                 browser.switch_to.window(original_window)
 
+        # 스크롤을 내리는 명령
         browser.execute_script('window.scrollTo(0, document.body.scrollHeight)')
         time.sleep(4)
 
